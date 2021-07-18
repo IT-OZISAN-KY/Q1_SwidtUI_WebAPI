82d29babfd8f29fac731742d33145d06b572944c
k-yamaki <k.yamaki@hiroshimamm.com>
2021-07-18 19:15:33 +0900

    Initial Commit

---
+++ Q1_SwidtUI_WebAPI/Q1_SwidtUI_WebAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Q1_SwidtUI_WebAPIApp.swift
+//  Q1_SwidtUI_WebAPI
+//
+//  Created by keiji yamaki on 2021/07/18.
+//
+
+import SwiftUI
+
+@main
+struct Q1_SwidtUI_WebAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
